8e7aab3ab3b4c0333520b6d803b82fd0bb4c9f86
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-07-16 22:23:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // complete this js code
 function Person(name, age) {}
-Person.prototype.greet = function(){
+Person.prototype.greet = function(name,age){
 	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
 }
 
 function Employee(name, age, jobTitle) {}
-Employee.prototype.jobGreet = function () {
+Employee.prototype.jobGreet = function (name, age, jobTitle) {
 	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
 }
 // Do not change code below this line
